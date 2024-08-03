1f5fc75127790d129c2aadbabc89204d356a236a
Vicente Pereira <vicente.pereira.ch@gmail.com>
2024-08-02 21:03:07 -0400

    changed URI variable

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-process.env.MONGODB_URI
+const URI = process.env.MONGODB_URI
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(URI)
 
     .then(db=>console.log("DB conectada"))
     .catch(err => console.log("Error"))
